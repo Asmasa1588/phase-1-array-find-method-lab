23e18ac57ed00f7996d35c77bc327b4a9d27fc2a
Asma <as1588ma@gmail.com>
2022-04-08 21:39:07 -0400

    commit tree 583f5094859b3624e7c6000685f0545aa537e8cc

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+  const result = record.find((currentRecord) => {
+    return currentRecord.result === "W";
+  });
+  if (result) {
+    return result.year;
+  }
+  return undefined;
+}
